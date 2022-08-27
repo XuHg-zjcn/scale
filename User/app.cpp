c04494619da1d57e03128dceaef04338b43eb1a1
徐瑞骏 <1687701765@qq.com>
2022-08-27 11:15:42 +0800

    HX711+SSD1306测试

--- User/app.cpp
+++ User/app.cpp
@@ -1,34 +1,37 @@
-/********************************************************************************
- * C++程序入口文件
- * Copyright (C) 2021-2022  Xu Ruijun
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *******************************************************************************/
 #include "app.h"
-#include "c_pin.hpp"
 #include "delay.hpp"
+#include "c_pin.hpp"
+#include "s_i2c.hpp"
+#include "hx711.hpp"
+#include "ssd1306.hpp"
 
+char str[20];
 
 int app(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	C_Pin led = C_Pin(2, 13);
-	led.loadXCfg(GPIO_GP_PP0);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+	C_Pin scl = C_Pin(1, 10);
+	C_Pin sda = C_Pin(1, 11);
+	scl.loadXCfg(GPIO_GP_OD1);
+	sda.loadXCfg(GPIO_GP_OD1);
+	S_I2C si2c = S_I2C(scl, sda);
+	si2c.set_clock(100000);
+	S_I2C_Dev dev = S_I2C_Dev(&si2c, Addr_OLED);
+	SSD1306 oled = SSD1306(&dev);
+	oled.Init();
+	oled.fill(0x00);
+
+	C_Pin sck = C_Pin((int)0, 10);
+	C_Pin dout = C_Pin((int)0, 9);
+	sck.loadXCfg(GPIO_GP_PP0);
+	dout.loadXCfg(GPIO_In_Float);
+	HX711 hx = HX711(sck, dout);
+	hx.Init(HX711_CHA_128);
 	while(1){
-		led.write_pin(Pin_Reset);
-		XDelayMs(100);
-		led.write_pin(Pin_Set);
-		XDelayMs(100);
+		snprintf(str, 20, "%8d", hx.block_raw());
+		oled.setVHAddr(Vert_Mode, 0, 127, 0, 0);
+		oled.text_5x7(str);
+		//XDelayMs(100);
 	}
 	return 0;
 }
--- User/mylibs_config.hpp
+++ User/mylibs_config.hpp
@@ -1,7 +1,11 @@
+#ifndef MYLIBS_CONFIG
+#define MYLIBS_CONFIG
 #include "myints.h"
 #include "debug.h"
 #include "x_base.hpp"
 #include <cstdlib>
+#define SSD1306_I2C_Dev S_I2C_Dev
 
 #define XMalloc(x) malloc(x)
 #define XFree(x)   free(x)
+#endif
